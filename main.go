0208fd19d0db51fe89e8920b2e6806e6da9a1235
waysaku <waysaku@gmail.com>
2018-03-13 09:30:14 +0800

    added bloomfilter lib test code

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
-	"./origin"
+	"github.com/willf/bloom"
+	"github.com/waysaku/bloomfilter/origin"
 	"fmt"
 )
 
 func main()  {
 	fmt.Println("START")
 	origin.Filter()
+
+	n := uint(1000)
+	filter := bloom.New(20 * n, 5) // load of 20, 5 keys
+	filter.Add([]byte("Love"))
+
+	if filter.Test([]byte("Love")) {
+		fmt.Println("DETECT!!")
+	}
+	if filter.Test([]byte("HOGE")) {
+		fmt.Println("DETECT!!")
+	}
 }
 
 
